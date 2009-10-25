b573440d3424ddd5acf81af1f343add25bf42db6
sugyan <sugi1982@gmail.com>
2009-10-25 20:50:36 +0900

    timelineの取得とコールバックを実装

--- lib/AnyEvent/Wassr.pm
+++ lib/AnyEvent/Wassr.pm
@@ -1,20 +1,43 @@
 package AnyEvent::Wassr;
 
 use base 'Object::Event';
+use AnyEvent::HTTP;
 use Carp 'croak';
+use JSON 'decode_json';
+use MIME::Base64;
 use Scalar::Util 'weaken';
+use Try::Tiny;
+use URI::URL;
 
 sub new {
    my $class = shift;
    my $self  = $class->SUPER::new(
        @_,
+       enable_methods => 1,
    );
 
-   if (!defined($self->{username})) {
+   # required arguments
+   if (!defined $self->{username}) {
        croak "no 'username' given to AnyEvent::Wassr\n";
    }
-   if (!defined($self->{password})) {
+   if (!defined $self->{password}) {
        croak "no 'password' given to AnyEvent::Wassr\n";
+   }
+
+   # default values
+   if (!defined $self->{base_url}) {
+       $self->{base_url} = 'http://api.wassr.jp';
+   }
+   if (!defined $self->{interval}) {
+       $self->{interval} = 60;
+   }
+   if (!defined $self->{state}) {
+       $self->{state} = {};
+   }
+
+   # invalid
+   if ($self->{interval} < 10) {
+       croak "invalid value of 'interval'. it must be more than 10\n";
    }
 
    return $self;
@@ -27,7 +50,9 @@ sub receive_statuses_friends {
     $self->{schedule}{statuses_friends} = {
         wait    => 0,
         weight  => $weight || 1,
-        request => sub {},
+        request => sub {
+            $self->_fetch_status_update('friends_timeline', @_);
+        },
     };
 }
 
@@ -38,7 +63,9 @@ sub receive_statuses_replies {
     $self->{schedule}{statuses_replies} = {
         wait    => 0,
         weight  => $weight || 1,
-        request => sub {},
+        request => sub {
+            $self->_fetch_status_update('replies', @_);
+        },
     };
 }
 
@@ -46,6 +73,9 @@ sub start {
     my $self = shift;
 
     $self->_tick;
+}
+
+sub error {
 }
 
 sub _tick {
@@ -67,6 +97,80 @@ sub _tick {
         sub { $self->_schedule_next_tick(shift) }, $max_task,
     );
     $max_task->{wait} = 0;
+}
+
+sub _fetch_status_update {
+    my ($self, $tl_name, $next_cb) = @_;
+
+    my $url = URI::URL->new($self->{base_url});
+    $url->path_segments('statuses', $tl_name . '.json');
+
+    weaken $self;
+    $self->{http_get}->{$statuses} = http_get(
+        $url->as_string,
+        headers => $self->_get_basic_auth,
+        sub {
+            my ($data, $headers) = @_;
+
+            delete $self->{http_get}{$statuses};
+            if ($headers->{Status} =~ /^2/) {
+                $self->event(
+                    $tl_name,
+                    $self->_analze_statuses($tl_name, $data),
+                );
+            } else {
+                $self->error(
+                    "error while fetching statuses for $statuses: "
+                        . "$headers->{Status} $headers->{Reason}");
+            }
+            $next_cb->($headers);
+        },
+    );
+}
+
+sub _schedule_next_tick {
+    my ($self, $headers) = @_;
+
+    $self->_tick and return if !defined $headers;
+    my $next_tick = $self->{interval};
+
+    weaken $self;
+    $self->{_tick_timer} = AnyEvent->timer(
+        after => $next_tick,
+        cb    => sub {
+            delete $self->{_tick_timer};
+            $self->_tick;
+        },
+    );
+}
+
+sub _get_basic_auth {
+    my $self = shift;
+
+    my $base64 = encode_base64("$self->{username}:$self->{password}");
+    return {
+        Authorization => "Basic $base64",
+    };
+}
+
+sub _analze_statuses {
+    my ($self, $tl_name, $raw_data) = @_;
+
+    my $data;
+    try {
+        $data = decode_json($raw_data);
+    } catch {
+        $self->error("error while parsing statuses for $status_name: $_");
+    };
+
+    my $state = ($self->{state}{statuses}{$tl_name} ||= {});
+    my @statuses = grep {
+        $_->{epoch} > $state->{epoch};
+    } @$data;
+
+    $state->{epoch} = $statuses[0]->{epoch} if @statuses;
+
+    return @statuses;
 }
 
 1;
