f73a09355e60cee1504aa39d84a389f1bcf29a02
sugyan <sugi1982@gmail.com>
2009-11-25 08:11:23 +0900

    初回起動時のデフォルト設定方法を変更

--- twiterm.pl
+++ twiterm.pl
@@ -3,20 +3,21 @@ use warnings;
 
 use FindBin;
 use lib File::Spec->catfile($FindBin::Bin, 'lib');
-use Net::Twitter::Lite;
+use Term::ReadLine;
+use Try::Tiny;
 use Twiterm;
 use YAML qw/DumpFile LoadFile/;
 
 my $twiterm = new Twiterm();
 
 my $config_path = $ARGV[0] || 'twiterm-config.yaml';
-# An example of config.yaml:
+# An example of twiterm-config.yaml:
 # ---
 # accounts:
 #   MyTwitter:
 #     service: twitter
-#     access_token: *************************************************
-#     access_token_secret: ******************************************
+#     username: hogehoge
+#     password: ********
 # pages:
 #   - name: friends' timeline
 #     account_id: MyTwitter
@@ -24,37 +25,102 @@ my $config_path = $ARGV[0] || 'twiterm-config.yaml';
 #   - name: mentions
 #     account_id: MyTwitter
 #     timeline: mentions
-my $config = eval {
+my $config;
+$config = try {
     LoadFile($config_path);
+} catch {
+    create_default_config();
 };
-if ($@) {
-    my $ntl = Net::Twitter::Lite->new(%$twiterm);
-    my $auth_url = $ntl->get_authorization_url;
-    print "Authorize this app at\n$auth_url\nand enter the PIN# :";
-    # wait for input
-    my $pin = <STDIN>;
-    chomp $pin;
-
-    my($access_token, $access_token_secret) = $ntl->request_access_token(verifier => $pin);
-    $config = {
-        accounts => {
-            MyTwitter => {
-                service  => 'twitter',
-                access_token        => $access_token,
-                access_token_secret => $access_token_secret,
-            },
-        },
-        pages => [{
-            name       => 'home timeline',
-            account_id => 'MyTwitter',
-            timeline   => 'friends',
-        }, {
-            name       => 'mentions',
-            account_id => 'MyTwitter',
-            timeline   => 'mentions',
-        }],
-    };
-    DumpFile($config_path, $config);
-}
 
 $twiterm->run($config);
+
+sub create_default_config {
+    my $config;
+    my $term = new Term::ReadLine;
+
+    my $use_twitter;
+  USE_TWITTER:
+    while (defined (my $input = $term->readline('use Twitter? (y/n) [y]: '))) {
+        if ($input eq '' || lc($input) =~ /^(y|n)$/) {
+            $use_twitter = lc($input) eq 'n' ? 0 : 1;
+            last USE_TWITTER;
+        }
+    }
+    while ($use_twitter) {
+        my $account = sprintf 'Twitter%d', scalar keys %{$config->{accounts}};
+        print "account: $account\n";
+        my ($username, $password);
+      USERNAME:
+        while (defined ($username = $term->readline('username: '))) {
+            last USERNAME if $username =~ /^\w+$/;
+        }
+      PASSWORD:
+        while (defined ($password = $term->readline('password: '))) {
+            last PASSWORD if $password =~ /^.+$/;
+        }
+        $config->{accounts}{$account} = {
+            service  => q/twitter/,
+            username => $username,
+            password => $password,
+        };
+        push @{$config->{pages}}, {
+            name       => qq/$account home timeline/,
+            account_id => $account,
+            timeline   => q/friends/,
+        };
+        push @{$config->{pages}}, {
+            name       => qq/$account mentions/,
+            account_id => $account,
+            timeline   => q/mentions/,
+        };
+
+      USE_OTHER:
+        while (defined (my $input = $term->readline('use other account? (y/n) [n]: '))) {
+            if ($input eq '' || lc($input) =~ /^(y|n)$/) {
+                $use_twitter = lc($input) eq 'y' ? 1 : 0;
+                last USE_OTHER;
+            }
+        }
+    }
+
+    my $use_wassr;
+  USE_WASSR:
+    while (defined (my $input = $term->readline('use Wassr? (y/n) [y]: '))) {
+        if ($input eq '' || lc($input) =~ /^(y|n)$/) {
+            $use_wassr = lc($input) eq 'n' ? 0 : 1;
+            last USE_WASSR;
+        }
+    }
+    if ($use_wassr) {
+        my $account = 'Wassr';
+        print "account: $account\n";
+        my ($username, $password);
+      USERNAME:
+        while (defined ($username = $term->readline('username: '))) {
+            last USERNAME if $username =~ /^\w+$/;
+        }
+      PASSWORD:
+        while (defined ($password = $term->readline('password: '))) {
+            last PASSWORD if $password =~ /^.+$/;
+        }
+        $config->{accounts}{$account} = {
+            service  => q/wassr/,
+            username => $username,
+            password => $password,
+        };
+        push @{$config->{pages}}, {
+            name       => qq/$account friends timeline/,
+            account_id => $account,
+            timeline   => q/friends/,
+        };
+        push @{$config->{pages}}, {
+            name       => qq/$account replies/,
+            account_id => $account,
+            timeline   => q/mentions/,
+        };
+    }
+
+    DumpFile($config_path, $config);
+
+    return $config;
+}
